3a60c43eba425565bc41f66ae72c6a47661ca187
Astroner <zhenyanikforov@gmail.com>
2020-03-04 00:44:23 +0300

    xor realization

--- src/eugine/XOREncoder.java
+++ src/eugine/XOREncoder.java
@@ -4,12 +4,21 @@ import common.Encoder;
 
 public class XOREncoder implements Encoder{
 
-	public String encode(String message, String secret) {
-
-		return "XOR encode";
+	public String encode(String str, String secret) {
+		return XOR(str, secret);
 	}
-	public String decode(String message, String secret) {
-
-		return "XOR decode";
+	
+	public String decode(String str, String secret) {
+		return XOR(str, secret);
+	}
+	
+	private String XOR(String str, String secret) {
+		char[] key = secret.toCharArray(); //Can be any chars, and any length array
+		String output = "";
+		
+		for(int i = 0; i < str.length(); i++) {
+			output += ((char) (str.charAt(i) ^ key[i % key.length]));
+		}
+		return output;
 	}
 }
